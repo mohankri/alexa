04473b353e7db657ccd21b70cf447913118df909
mohankri <mohankri@gmail.com>
2017-07-09 16:18:42 -0700

    Added Support for Order

--- sensitel/src/index.js
+++ sensitel/src/index.js
@@ -19,19 +19,24 @@ var handlers = {
     'OrderIntent': function () {
         var itemSlot = this.event.request.intent.slots.Order;
 	var itemName;
+	var itemSlot;
 	if (itemSlot && itemSlot.value) {
 		itemName = itemSlot.value.toLowerCase();
+		itemSlot = itemSlot.name.toLowerCase();
 	}
-        var query = this.t("GREETING_QUERY", this.t("SKILL_NAME"), itemName);
+	console.log("itemName " + itemName);
+	console.log("itemSlot " + itemSlot);
+        var query = this.t("ORDER_QUERY", this.t("SKILL_NAME"), itemName);
 	console.log('Query for ' + query);
 	var translate = this.t("SENSITEL");
-	var item = translate[itemName];
+	var item = translate[itemSlot];
 	console.log('item  = ' + item);
 	if (item) {
 		console.log('item  found = ' + item);
-		this.attributes['speechOutput'] = item;
+		var result = item + itemName + " is completed";
+		this.attributes['speechOutput'] = result;
 		this.attributes['repromptSpeech'] = this.t("SENSITEL_REPEAT_MESSAGE");
-		this.emit(':tellWithCard', item, this.attributes['repromptSpeech'], query, item);
+		this.emit(':tellWithCard', result, this.attributes['repromptSpeech'], query, item);
 	} else {
 		console.log('item  not found = ' + item);
 		var speechOutput = this.t("SENSITEL_NOT_FOUND_MESSAGE");
@@ -80,6 +85,7 @@ var languageStrings = {
                 "SENSITEL": queries.SENSITEL_EN_US,
                 "SKILL_NAME": "Sensitel",
                 "GREETING_QUERY": "%s  - sensitel  %s.",
+                "ORDER_QUERY": "sensitel %s.",
                 "SENSITEL_REPEAT_MESSAGE": "Try saying repeat.",
                 "SENSITEL_NOT_FOUND_MESSAGE": "I\'m sorry Sensitel do not know",
                 "SENSITEL_NOT_FOUND_REPROMPT": "What else can I help with ?",
