40c4be6ba92a50c1751dbbb4e3c472655337c001
mohankri <mohankri@gmail.com>
2017-07-10 17:23:24 -0700

    Fix Greeting

--- sensitel/src/index.js
+++ sensitel/src/index.js
@@ -11,6 +11,7 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
+
 var handlers = {
     'LaunchRequest': function () {
         this.emit('SayHello');
@@ -18,8 +19,7 @@ var handlers = {
 
     'OrderIntent': function () {
         var itemSlot = this.event.request.intent.slots.Order;
-	var itemName;
-	var itemSlot;
+	var itemName, itemSlot;
 	if (itemSlot && itemSlot.value) {
 		itemName = itemSlot.value.toLowerCase();
 		itemSlot = itemSlot.name.toLowerCase();
@@ -30,13 +30,14 @@ var handlers = {
 	console.log('Query for ' + query);
 	var translate = this.t("SENSITEL");
 	var item = translate[itemSlot];
-	console.log('item  = ' + item);
+	//console.log('item  = ' + item);
 	if (item) {
-		console.log('item  found = ' + item);
+		//console.log('item  found = ' + item);
 		var result = item + itemName + " is completed";
 		this.attributes['speechOutput'] = result;
 		this.attributes['repromptSpeech'] = this.t("SENSITEL_REPEAT_MESSAGE");
-		this.emit(':tellWithCard', result, this.attributes['repromptSpeech'], query, item);
+		this.emit(':tellWithCard', this.attributes['speechOutput'], 
+				this.attributes['repromptSpeech'], query, item);
 	} else {
 		console.log('item  not found = ' + item);
 		var speechOutput = this.t("SENSITEL_NOT_FOUND_MESSAGE");
@@ -74,8 +75,16 @@ var handlers = {
     },
 
     'SayHello': function () {
-        this.emit(':tell', 'Greeting from Sensitel.');
+        //this.emit(':tell', 'Greeting from Sensitel.');
+       	var query = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
+        this.emit(':tell', query);
+    },
+
+    'AboutIntent': function() {
+         var speechOutput = "I'm developed by Sensitel";
+	 this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), speechOutput);
     }
+
 };
 
 
@@ -90,7 +99,8 @@ var languageStrings = {
                 "SENSITEL_NOT_FOUND_MESSAGE": "I\'m sorry Sensitel do not know",
                 "SENSITEL_NOT_FOUND_REPROMPT": "What else can I help with ?",
                 "SENSITEL_NOT_FOUND_ITEM_NAME": "Query for %s. ",
-                "SENSITEL_NOT_FOUND_WITHOUT_ITEM_NAME": "that query"
+                "SENSITEL_NOT_FOUND_WITHOUT_ITEM_NAME": "that query",
+		"WELCOME_MESSAGE": "Welcome to %s " 
         }
     },
     "en-US": {
